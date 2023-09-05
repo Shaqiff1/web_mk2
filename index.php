762239ce7d4d03d848b2862e09cba48663c7f331
ELVINJIT SINGH <126230593+ELVINJIT@users.noreply.github.com>
2023-09-05 15:45:46 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ceritakan tentang diri anda</title>
+</head>
+<body>
+<h1>Ceritakan tentang diri anda:</h1>
+<form action="display.php" method="post">
+    <textarea name="cerita" rows="5" cols="40"></textarea>
+    <br>
+    <input type="submit" name="simpan" value="Simpan">
+</form>
+</body>
+</html>
+
